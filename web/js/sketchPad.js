08d140f48073eca2ee53ca6ffc90be3b7e5d5405
Andre Burke <aburke.dev@gmail.com>
2023-07-05 01:49:15 -0600

    initial commit

---
+++ web/js/sketchPad.js
@@ -0,0 +1,55 @@
+class SketchPad {
+  constructor(container, size = 400) {
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = size;
+    this.canvas.height = size;
+    this.canvas.style = `
+            background-color: white;
+            box-shadow: 0px 0px 10px 2px black;
+        `;
+
+    container.appendChild(this.canvas);
+
+    this.ctx = this.canvas.getContext("2d");
+
+    this.path = [];
+    this.isDrawing = false;
+
+    this.#addEventListeners();
+  }
+
+  #addEventListeners() {
+    this.canvas.onmousedown = (e) => {
+      const mouse = this.#getMouse(e);
+      this.path = [mouse];
+      this.isDrawing = true;
+    };
+
+    this.canvas.onmousemove = (e) => {
+      if (this.isDrawing) {
+        const mouse = this.#getMouse(e);
+        this.path.push(mouse);
+        this.#redraw();
+      }
+    };
+
+    this.canvas.onmouseup = () => {
+      this.isDrawing = false;
+    };
+
+    let btn = document.querySelector("#clear");
+
+    btn.addEventListener("click", () => {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    });
+  }
+
+  #redraw() {
+    draw.path(this.ctx, this.path);
+  }
+
+  #getMouse(e) {
+    const rect = this.canvas.getBoundingClientRect();
+    return [Math.round(e.clientX - rect.left), Math.round(e.clientY - rect.top)];
+  }
+}
