ce8d5b564e01864e6344683366382b6d7da44449
Andre Burke <aburke.dev@gmail.com>
2023-07-22 21:49:07 -0600

    added a group by function in utils

--- common/utils.js
+++ common/utils.js
@@ -13,6 +13,22 @@ utils.printProgress = (count, max) => {
   process.stdout.write(`${count}/${max} (${percent})`);
 };
 
+utils.groupBy = (samples, studentIdKey) => {
+  const studentGrouping = {};
+
+  for (let student of samples) {
+    const studentId = student[studentIdKey];
+
+    if (studentGrouping[studentId] == null) {
+      studentGrouping[studentId] = [];
+    }
+
+    studentGrouping[studentId].push(student);
+  }
+
+  return studentGrouping;
+};
+
 if (typeof module !== "undefined") {
   module.exports = utils;
 }
